853bfc6fbc94cd1018296aab9d8352a4c622137f
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-11 10:12:29 -0300

    Instlado o VerifyJS para validação async

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../plugins/verify.notify")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -27,6 +28,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
