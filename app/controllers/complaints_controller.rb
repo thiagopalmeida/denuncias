eec6b7f7ba6c77e21bdd9e8147672efc0822de39
Ana Paula Tavares Pontello Neves <anapaulapontello@gmail.com>
2020-12-12 09:37:59 -0300

    Ajustes form_retirei comentarios desnecessarios

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -22,11 +22,6 @@ class ComplaintsController < ApplicationController
     @complaint_categories_ids.each do |id|
       new = ComplaintCategory.new(category_id: id.to_i, complaint_id: @complaint.id)
       new.save
-      # Só conseguimos visualizar as categorias quando criamos o complaint_categories_id no model da Complaints.
-      # Mas as categorias vinculadas a cada complain devem ser acessadas por @complaint.categories e não por @complaint.complaint_categories_id
-
-      # @complaint.complaint_categories_id = new.id
-      # @complaint.save
     end
     redirect_to root_path, notice: "Denúncia criada com sucesso!"
   end
