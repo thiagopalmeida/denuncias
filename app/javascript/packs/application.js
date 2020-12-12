4f444981551e98cd565e83fd50b9da748280ce06
thiago almeida <thiagopalmeida@gmail.com>
2020-12-12 00:55:51 -0300

    Adicionado o typed na home, mas falta finalizar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("../plugins/verify.notify")
 // External imports
 import "bootstrap";
 // import Typed from 'typed.js';
+import "../plugins/typed"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
