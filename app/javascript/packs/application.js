ae1f3d1eacdca180a5964acdc1a12328244621a2
Ana Paula Tavares Pontello Neves <anapaulapontello@gmail.com>
2020-12-14 11:13:50 -0300

    help page - não finalizada

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,7 +30,6 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_182033) do
+ActiveRecord::Schema.define(version: 2020_12_12_122842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
