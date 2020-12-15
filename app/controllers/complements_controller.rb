6b1bc31f02df7edec0501fac97405930c8f8ac8c
Millena Wiese <millenawiese@gmail.com>
2020-12-15 12:34:47 -0300

    Corrigindo complemento da denúncia

--- app/controllers/complements_controller.rb
+++ app/controllers/complements_controller.rb
@@ -8,7 +8,6 @@ class ComplementsController < ApplicationController
     @complaint = Complaint.find(params[:complaint_id])
     @complement = Complement.new(complement_params)
     @complement.complaint = @complaint
-    @complement.user = current_user
     if @complement.save
       redirect_to @complaint, notice: 'O complemento de sua denúncia foi recebido e será encaminhado para análise. Obrigado.'
     else
