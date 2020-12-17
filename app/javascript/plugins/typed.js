9e2edc68aa09606c6a2aad469b7fe1a5ed14786c
thiago almeida <thiagopalmeida@gmail.com>
2020-12-17 11:16:36 -0300

    Adaptação home com foto ao inves de video para Heroku

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -5,7 +5,7 @@ const loadTyped = () => {
       new Typed('#typed', {
         strings: [" ao descaminho ", " ao contrabando ", " à corrupção ", " à contrafação ", " à sonegação "],
         typeSpeed: 80,
-        startDelay: 17500,
+        startDelay: 2000,
         fadeOut: true,
         showCursor: false,
         smartBackspace: true,
